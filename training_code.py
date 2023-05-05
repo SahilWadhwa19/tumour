93fb2c8b00d39873b7cd5187afcc960cdebb7aba
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2023-05-05 11:03:55 +0000

    Update training_code.py

--- training_code.py
+++ training_code.py
@@ -280,6 +280,8 @@ with mlflow.start_run(run_name="tumour") as run:
     mlflow.keras.log_model(keras_model=model, artifact_path=None)
     """
 model.fit(images_data, masks_data, epochs = 4, batch_size = 1, validation_split=0.1, callbacks=[loggingCallback()])
+print("Data type is ", images_data.dtype)
+images_data = np.float32(images_data)
 y_pred = model.predict(images_data)
 y_test = mask_data
 print("ConfMat y_test ",y_test.shape, y_test.dtype)
