01bee65b769d7fa8905d40f3c72ccd1f72e0b9d2
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-25 16:08:42 +0000

    Update and rename HPC_Chatbot_v4.5.py to HPC_Chatbot_v5.3.py

--- HPC_Chatbot_v5.3.py
+++ HPC_Chatbot_v5.3.py
@@ -5,7 +5,7 @@ from langchain_ollama import ChatOllama
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-# from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 class Pipeline:
     
@@ -35,28 +35,7 @@ class Pipeline:
             timeout=None,
             max_retries=2,
         )
-        os.environ["GOOGLE_API_KEY"]="AIzaSyDf5jdwzdhEpjip3aEB0sywg9htgYy3RUA"
-        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-        """
-        self.database=FAISS.load_local(
-        "/app/faiss_index_latest_db_6", GoogleGenerativeAIEmbeddings(model="models/embedding-001"), allow_dangerous_deserialization=True
-        )
-        """
-        from langchain_core.prompts import ChatPromptTemplate
         
-        self.prompt = ChatPromptTemplate.from_template("""
-        You are an experienced HPC and Datacenter Solutions Presales Engineer. You provide insights and assistance to other engineers and sales persons to enable them to find appropriate products and solutions from our portfolio of products and roadmaps provided in the augmented data set. 
-        
-        You should try to be as accurate as possible, but provide potential solutions if you are unable to find sufficient data, but explain if suggestions may require further confirmation and development if presented.
-        
-        <context>
-        {context}
-        </context>
-        Question: {input}""")
-
-        self.document_chain=create_stuff_documents_chain(self.llm,self.prompt)
-        self.retriever=self.database.as_retriever()
-        self.retrieval_chain=create_retrieval_chain(self.retriever,self.document_chain)
         pass
         
         
