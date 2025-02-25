ac7771453aceea96ff0b0b349c6b3c087a5a02d2
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-25 22:33:38 +0000

    Update and rename Supermicro_Chatbot_v8.py to Supermicro_Chatbot_v9.py

--- Supermicro_Chatbot_v9.py
+++ Supermicro_Chatbot_v9.py
@@ -72,4 +72,4 @@ class Pipeline:
         retrieval_chain=create_retrieval_chain(retriever,document_chain)
         response = retrieval_chain.invoke({"input":user_message})
         # response = llm.invoke(user_message)
-        return response["answer"]
+        return "Found some great answers"
