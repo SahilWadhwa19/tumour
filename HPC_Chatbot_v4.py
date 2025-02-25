ba7a357d5d7ba8d17261fcea0b6aca8fe61efd73
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-25 15:42:10 +0000

    Update and rename HPC_Chatbot_v3.1.py to HPC_Chatbot_v4.py

--- HPC_Chatbot_v4.py
+++ HPC_Chatbot_v4.py
@@ -15,6 +15,10 @@ class Pipeline:
     def __init__(self):
         self.llm = None
         self.database = None
+        self.prompt = None
+        self.document_chain = None
+        self.retriever = None
+        self.retrieval_chain = None
         self.valves = self.Valves(
             **{
                 "MODEL_NAME": os.getenv("MODEL_NAME", "llama3-70b-8192"),
@@ -23,7 +27,7 @@ class Pipeline:
         
     async def on_startup(self):
         os.environ["GROQ_API_KEY"] = "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"
-        global llm, database
+        global llm, database, prompt, document_chain, retriever, retrieval_chain
         self.llm = ChatGroq(
             model=self.valves.MODEL_NAME,
             temperature=0.7,
@@ -35,7 +39,20 @@ class Pipeline:
         embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         self.database=FAISS.load_local(
         "faiss_index_latest_db_6", GoogleGenerativeAIEmbeddings(model="models/embedding-001"), allow_dangerous_deserialization=True
-    )
+        )
+        self.prompt = ChatPromptTemplate.from_template("""
+        You are an experienced HPC and Datacenter Solutions Presales Engineer. You provide insights and assistance to other engineers and sales persons to enable them to find appropriate products and solutions from our portfolio of products and roadmaps provided in the augmented data set. 
+        
+        You should try to be as accurate as possible, but provide potential solutions if you are unable to find sufficient data, but explain if suggestions may require further confirmation and development if presented.
+        
+        <context>
+        {context}
+        </context>
+        Question: {input}""")
+
+        self.document_chain=create_stuff_documents_chain(self.llm,self.prompt)
+        self.retriever=self.database.as_retriever()
+        self.retrieval_chain=create_retrieval_chain(self.retriever,self.document_chain)
         pass
         
         
