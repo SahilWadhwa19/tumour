299af7834896cd516ea8071cc097e0863d36c83c
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-26 14:58:16 +0000

    Update and rename Supermicro_Chatbot_create_65.py to Supermicro_Chatbot_create_66.py

--- Supermicro_Chatbot_create_66.py
+++ Supermicro_Chatbot_create_66.py
@@ -79,4 +79,4 @@ class Pipeline:
         # response = self.llm.invoke(user_message)
         # Print the current working directory
         print("Current working directory:", cwd)
-        return str(type(self.document_chain))
+        return str(type(self.retriever))
