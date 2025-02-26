7bca3bd4a611a7fed5f907d2e58a658b50c9f57b
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-26 14:54:24 +0000

    Update and rename Supermicro_Chatbot_create_63.py to Supermicro_Chatbot_create_64.py

--- Supermicro_Chatbot_create_64.py
+++ Supermicro_Chatbot_create_64.py
@@ -75,7 +75,7 @@ class Pipeline:
         print(user_message)
         
         cwd = os.getcwd()
-        response = self.retrieval_chain.invoke({"input":user_message})
+        # response = self.retrieval_chain.invoke({"input":user_message})
         # response = self.llm.invoke(user_message)
         # Print the current working directory
         print("Current working directory:", cwd)
