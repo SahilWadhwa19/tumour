76a117809e326637772f662108fe29f3fff349d1
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-27 10:55:16 +0000

    Update and rename Supermicro_Chatbot_create_HPC_file_v2.py to Supermicro_Chatbot_create_HPC_file_v3.py

--- Supermicro_Chatbot_create_HPC_file_v3.py
+++ Supermicro_Chatbot_create_HPC_file_v3.py
@@ -63,7 +63,8 @@ class Pipeline:
         self.retrieval_chain=create_retrieval_chain(self.retriever,self.document_chain)
         # self.response = self.retrieval_chain.invoke()
         # self.sample_data = "All will be great"
-        self.response = self.database.index.ntotal
+        # self.response = self.database.index.ntotal
+        self.response = self.database.similarity_search("Great features")[0].page_content
         pass
         
         
