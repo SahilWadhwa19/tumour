064c15131e9f08985d949cfe770f0cf44e4b5cd8
SahilWadhwa19 <131771349+SahilWadhwa19@users.noreply.github.com>
2025-02-27 10:44:25 +0000

    Update and rename Supermicro_Chatbot_create_82.py to Supermicro_Chatbot_create_83.py

--- Supermicro_Chatbot_create_83.py
+++ Supermicro_Chatbot_create_83.py
@@ -24,7 +24,7 @@ class Pipeline:
         self.document_chain = None
         self.retriever = None
         self.retrieval_chain = None
-        self.response = None
+        # self.response = None
         # self.input = None
         self.valves = self.Valves(
             **{
@@ -44,7 +44,7 @@ class Pipeline:
         os.environ["GOOGLE_API_KEY"]="AIzaSyDf5jdwzdhEpjip3aEB0sywg9htgYy3RUA"
         self.embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
         self.database=FAISS.load_local(
-        "/app/faiss_index_latest_db_6", GoogleGenerativeAIEmbeddings(model="models/embedding-001"), allow_dangerous_deserialization=True
+        "/app/faiss_index_latest_db_6", self.embeddings, allow_dangerous_deserialization=True
         )
         
         self.prompt = ChatPromptTemplate.from_template("""
